8deafd3e0c5b57f800a9c3f8d9cd1e3b5aca21cd
Bo Maryniuk <bo@suse.de>
2011-08-19 11:49:59 +0200

    Added Apache 2.0 header.

--- src/de/suse/lib/sqlmap/drivers/H2EmbeddedDriver.java
+++ src/de/suse/lib/sqlmap/drivers/H2EmbeddedDriver.java
@@ -1,5 +1,17 @@
 /*
- * Driver for embedded H2.
+ * Copyright 2011 SUSE Linux Products GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package de.suse.lib.sqlmap.drivers;
@@ -10,8 +22,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
+ * Driver operator for the H2 database in embedded mode.
  *
- * @author bo
+ * @author Bo Maryniuk
  */
 public class H2EmbeddedDriver extends GenericDriver {
     private final static String PROTO = "jdbc:h2://";
